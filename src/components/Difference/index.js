4765ee838bfe49750dc8ece3f55ef5428b213764
kitze <kristijan.mkd@gmail.com>
2018-10-28 15:51:52 +0100

    remove readme,

--- src/components/Difference/index.js
+++ src/components/Difference/index.js
@@ -48,7 +48,7 @@ function Difference({ processorValues }) {
         do {
           const firstPrice = firstProcessor.finalValue;
           const secondPrice = secondProcessor.finalValue;
-          const difference = Math.abs(firstPrice - secondPrice).toFixed(2);
+          const difference = (firstPrice - secondPrice).toFixed(2);
           <Fragment>
             <A.Space size={3} />
             <S.Value>$ {difference}</S.Value>
